8df4b37d940f71eb6b83551efe3ae8fb95c72e29
Maro <rexex.webdev@gmail.com>
2018-07-12 19:15:58 +0200

    cosmetic changes

--- script/navpush.js
+++ script/navpush.js
@@ -1,20 +1,27 @@
+  //function to OOP  !!! REBUILD
+  if (true){
   var sidenav = document.getElementById("mySidenav");
   var main = document.getElementById("main");
   var btn = document.getElementById("bar_container");
-  var cls_btn = document.getElementById("mySidenav");
+  // var cls_btn = document.getElementById("changed");
 
   function openNav() {
-    sidenav.style.width = "250px";
-    main.style.marginLeft = "-250px";
-    btn.classList.add("changed");
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  function closeNav() {
+    if (document.getElementsByClassName("changed")[0]){
       sidenav.style.width = "0";
       main.style.marginLeft = "0";
       btn.classList.remove("changed");
       document.body.style.backgroundColor = "white";
+    } else {
+    sidenav.style.width = "250px";
+    main.style.marginLeft = "-250px";
+    btn.classList.add("changed");
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
   }
+  // function closeNav() {
+  // 
+  // }
 
   btn.addEventListener('click', openNav, false);
-  cls_btn.addEventListener('click', closeNav, false);
+  // cls_btn.addEventListener('click', closeNav, false);
+}
