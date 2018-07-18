a6eee24d20c26723833740dda519220b940b508d
Maro <rexex.webdev@gmail.com>
2018-07-18 20:19:01 +0200

    - flexbox

--- script/navpush.js
+++ script/navpush.js
@@ -3,25 +3,48 @@
   var sidenav = document.getElementById("mySidenav");
   var main = document.getElementById("main");
   var btn = document.getElementById("bar_container");
-  // var cls_btn = document.getElementById("changed");
+  var wIw = window.innerWidth;
+  var isOpen = false;
+  
+  
+  btn.addEventListener('click', toogleNav, false);
 
-  function openNav() {
-    if (document.getElementsByClassName("changed")[0]){
+  $(window).resize(function(){
+    if ((wIw >= 970) && (isOpen == true)){
+      closeNav();
+    }
+  });
+  
+
+  
+  function toogleNav(){
+    let subMenu = document.getElementsByClassName("changed")[0];
+    if(subMenu){
+      closeNav();
+    } else {      
+      openNav();
+    }    
+  }
+  
+  function closeNav() {
       sidenav.style.width = "0";
       main.style.marginLeft = "0";
       btn.classList.remove("changed");
-      document.body.style.backgroundColor = "white";
-    } else {
+      document.body.style.backgroundColor = "honeydew";
+      isOpen = false;
+    } 
+    
+  function openNav() {
     sidenav.style.width = "250px";
     main.style.marginLeft = "-250px";
     btn.classList.add("changed");
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    }
+    isOpen = true;   
   }
-  // function closeNav() {
-  // 
-  // }
 
-  btn.addEventListener('click', openNav, false);
-  // cls_btn.addEventListener('click', closeNav, false);
+
+
+  
+  
+  
 }
