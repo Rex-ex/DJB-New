6762e444470fd8622317e39917d97fa96528960b
Maro <rexex.webdev@gmail.com>
2019-10-09 20:00:02 +0200

    - backBone, add video, CSSGrid

--- script/navpush.js
+++ script/navpush.js
@@ -1,43 +1,37 @@
 //function to OOP  !!! REBUILD //click outside close nav
-if (true){
-  const sidenav = document.getElementById('mySidenav')
-  const main = document.getElementById('mainCon')
-  const btn = document.getElementById('bar_container')
-  let isOpen = false  
-  btn.addEventListener('click', toogleNav, false)
-  
-  $(document).ready(function(){
-    const win = $(window)
-    win.resize(function(){
-      const wIw = win.innerWidth()
-      if ((wIw >= 970) && (isOpen == true)){
-        closeNav()
-      }
-    })
-  })
-  
-  function toogleNav(){
-    const subMenu = document.getElementsByClassName('changed')[0]
-    if(subMenu){
-      closeNav()
-    } else {      
-      openNav()
-    }    
+$(document).ready(function() {
+  const sidenav = document.getElementById("mySidenav");
+  const main = document.getElementsByClassName("my-wrapper")[0];
+  const btn = document.getElementById("bar_container");
+  const $blacked = $("#blacked");
+
+  let isOpen = false;
+  btn.addEventListener("click", toogleNav, false);
+
+  function toogleNav() {
+    const subMenu = document.getElementsByClassName("changed")[0];
+    if (subMenu) {
+      closeNav();
+    } else {
+      openNav();
+    }
   }
-  
+
   function closeNav() {
-    sidenav.style.width = '0'
-    main.style.marginLeft = '0'
-    btn.classList.remove('changed')
-    document.body.style.backgroundColor = 'honeydew'
-    isOpen = false
-  } 
-  
-  function openNav(){
-    sidenav.style.width = '250px'
-    main.style.marginLeft = '-250px'
-    btn.classList.add('changed')
-    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)'
-    isOpen = true
+    sidenav.style.width = "0";
+    main.style.marginLeft = "0";
+    btn.classList.remove("changed");
+    // $blacked.toggle();
+    $blacked.css("opacity", "0");
+    isOpen = false;
   }
-}
+
+  function openNav() {
+    sidenav.style.width = "250px";
+    // main.style.marginLeft = "-250px";
+    btn.classList.add("changed");
+    // $blacked.toggle();
+    $blacked.css("opacity", "0.8");
+    isOpen = true;
+  }
+});
--- script/scrollMenu.js
+++ script/scrollMenu.js
@@ -1,10 +1,10 @@
-// join two scripsts scroll, navpush to one OOP file
-$(document).ready(function(){
+// TO DO: join two scripsts scroll, navpush to one OOP file !!
+$(document).ready(function() {
   const $win = $(window);
   const $jumbo = $("#jumbo");
-  const $banner = $('#banner');
+  const $mainNav = $("#mainNav");
   const jumboH = $jumbo.height();
-  const brkPoint = (jumboH * 0.04);
+  const brkPoint = jumboH * 0.04;
   let scrolling = false;
 
   $win.scroll(function() {
@@ -14,16 +14,16 @@ $(document).ready(function(){
   setInterval(function() {
     if (scrolling) {
       scrolling = false;
-      if ($win.innerWidth() >= 970){
-        let scrlCurrent = $banner.offset();
-        if (scrlCurrent.top > brkPoint){
-          $banner.css("background", "grey");
-          $banner.css("opacity", "0.9");
+      if ($win.innerWidth() >= 970) {
+        let scrlCurrent = $mainNav.offset();
+        if (scrlCurrent.top > brkPoint) {
+          $main - nav.css("background", "grey");
+          $mainNav.css("opacity", "0.9");
         } else {
-          $banner.css("background", "none");
-          $banner.css("opacity", "1");
-        };
-      };
-    };
+          $mainNav.css("background", "none");
+          $mainNav.css("opacity", "1");
+        }
+      }
+    }
   }, 250);
 });
