63d5b59473211f6dd7e0aec9791e5079bbc0e700
Maro <rexex.webdev@gmail.com>
2019-11-06 17:50:07 +0100

    - arrows with offer

--- script/navpush.js
+++ script/navpush.js
@@ -4,14 +4,18 @@ $(document).ready(function() {
   // const main = document.getElementsByClassName("my-wrapper")[0];
   const main = document.getElementsByClassName("my-container--jumbo")[0];
   const btn = document.getElementById("bar_container");
+  const subMenu = document.getElementById("mySidenav__subMenu");
+  const subMenuActive = document.getElementsByClassName(
+    "mySidenav__subMenu--is-active"
+  );
   const $blacked = $("#blacked");
 
   let isOpen = false;
   btn.addEventListener("click", toogleNav, false);
 
   function toogleNav() {
-    const subMenu = document.getElementsByClassName("changed")[0];
-    if (subMenu) {
+    const buttonMenu = document.getElementsByClassName("changed")[0];
+    if (buttonMenu) {
       closeNav();
     } else {
       openNav();
@@ -22,6 +26,7 @@ $(document).ready(function() {
     sidenav.style.width = "0";
     main.style.marginLeft = "0";
     btn.classList.remove("changed");
+    subMenu.classList.remove("subMenu-isActive");
     // $blacked.toggle();
     $blacked.css("opacity", "0");
     isOpen = false;
@@ -31,6 +36,7 @@ $(document).ready(function() {
     sidenav.style.width = "250px";
     // main.style.marginLeft = "-250px";
     btn.classList.add("changed");
+    subMenu.classList.add("subMenu-isActive");
     // $blacked.toggle();
     $blacked.css("opacity", "0.8");
     isOpen = true;
