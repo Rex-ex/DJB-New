f229f866a8e2dad0301aec24d3071015758539d9
Maro <rexex.webdev@gmail.com>
2020-02-29 14:31:21 +0100

    - jumbo sub, gallery fix

---
+++ script/fancyboxOptions.js
@@ -0,0 +1,12 @@
+$('[data-fancybox="gallery"]').fancybox({
+  keyboard: true,
+  arrows: true,
+  transitionEffect: "circular",
+  buttons: [
+  "zoom",
+  "slideShow",
+  "fullScreen",
+  "thumbs",
+  "close"
+  ],
+});
