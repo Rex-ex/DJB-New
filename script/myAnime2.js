bece20a0e5b1ce553e61a0822ac44cc777dfdbe8
Maro <rexex.webdev@gmail.com>
2020-03-28 11:10:12 +0100

    - myanime, preloader 1fix

--- script/myAnime2.js
+++ script/myAnime2.js
@@ -1,34 +1,37 @@
-const $filters = $(".filters");
-const $nav = $(".mainNav");
-const $hero = $(".hero");
-const $menu = $(".bar_container");
-const $flags = $(".flags__container");
-$nav.fadeOut();
-$hero.fadeOut();
-$menu.fadeOut();
-$flags.fadeOut();
-// anime.js
-$(document).ready(function() {
-  // $preloaderDiv.fadeOut();
-  doAnimeEvent();
-  // if (animeContainer) {
-  function doAnimeEvent() {
+// "use strict";
+if (true) {
+  $("html").addClass("isActiveJS");
+  // do only is JS avalible / work in progress
+  $(window).on("load", function() {
     const $preloaderDiv = $(".filters__preloader");
-    const animStrips = ".anime__container span";
     const $filters = $(".filters");
     const $nav = $(".mainNav");
     const $hero = $(".hero");
     const $menu = $(".bar_container");
     const $flags = $(".flags__container");
-    // const $hamburger = $("nav");
-    const animeContainer = ".anime__container";
+    const $hamburger = $("nav");
     // $preloaderDiv.fadeOut();
     $nav.fadeOut();
     $hero.fadeOut();
     $menu.fadeOut();
     $flags.fadeOut();
-    // $hamburger.fadeOut();
+    $hamburger.fadeOut();
+    let fadeOut = setTimeout(function() {
+      $preloaderDiv.fadeOut();
+    }, 3000);
+    let animStart = setTimeout(function() {
+      doAnimeEvent();
+    }, 3001);
+  });
 
+  function doAnimeEvent() {
+    const animStrips = ".anime__container span";
+    const animeContainer = ".anime__container";
+    const $nav = $(".mainNav");
+    const $hero = $(".hero");
+    const $menu = $(".bar_container");
+    const $flags = $(".flags__container");
+    const $hamburger = $("nav");
     let tl = anime.timeline({
       easing: "easeOutExpo",
       duration: 750
@@ -53,12 +56,12 @@ $(document).ready(function() {
         rotate: "45deg",
         duration: 5000
       });
-    let fadeOut = setTimeout(function() {
+    let showElements = setTimeout(function() {
       $nav.fadeIn();
       $hero.fadeIn();
       $menu.fadeIn();
       $flags.fadeIn();
-    }, 3500);
+      $hamburger.fadeIn();
+    }, 4000);
   }
-  // }
-});
+}
--- script/pre-loader.js
+++ script/pre-loader.js
@@ -1,19 +1,40 @@
 if (true) {
   $("html").addClass("isActiveJS");
+  // const $filters = $(".filters");
+  // const $nav = $(".mainNav");
+  // const $hero = $(".hero");
+  // const $menu = $(".bar_container");
+  // const $flags = $(".flags__container");
+  // $nav.fadeOut();
+  // $hero.fadeOut();
+  // $menu.fadeOut();
+  // $flags.fadeOut();
   $(window).on("load", function() {
     const $preloaderDiv = $(".filters__preloader");
     const $filters = $(".filters");
+    // let fadeOut = setTimeout(function() {
     // $preloaderDiv.fadeOut();
-    let fadeOut = setTimeout(function() {
-      $preloaderDiv.fadeOut();
-    }, 3000);
+    // }, 3000);
     let hidePreloader = setTimeout(function() {
+      $preloaderDiv.fadeOut();
       $preloaderDiv.css("display", "none");
-    }, 3500);
+    }, 500);
     // temporry solution | Promise?
     // let disFilters = setTimeout(function() {
-    // $filters.css("display", "none");
-
+    // $preloaderDiv.css("display", "none");
     // }, 3500);
+    // const $preloaderDiv = $(".filters__preloader");
+    // const $filters = $(".filters");
+    // const $nav = $(".mainNav");
+    // const $hero = $(".hero");
+    // const $menu = $(".bar_container");
+    // const $flags = $(".flags__container");
+    // const $hamburger = $("nav");
+    // $preloaderDiv.fadeOut();
+    // $nav.fadeOut();
+    // $hero.fadeOut();
+    // $menu.fadeOut();
+    // $flags.fadeOut();
+    // $hamburger.fadeOut();
   });
 }
